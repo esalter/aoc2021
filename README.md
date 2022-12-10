91ab07aac62250ed9e80486612f87226ced71603
Eric Salter <mewsiclovr@gmail.com>
2022-12-10 17:32:10 -0500

    Day 5 completed

--- README.md
+++ README.md
@@ -15,7 +15,7 @@
 [![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
 [![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](src/day03)
 [![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%85/green)](src/day04)
-![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/05/%E2%98%85%E2%98%85/green)](src/day05)
 ![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/07/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/08/%E2%98%86%E2%98%86/gray)
@@ -90,16 +90,16 @@ Both parts: 11.11819ms
 
 ```
 Day 04
-Time part 1: 503.137ms
-Time part 2: 825.607ms
-Both parts: 1328.743209ms
+Time part 1: 15.832ms
+Time part 2: 9.156ms
+Both parts: 24.987684ms
 ```
 
 ```
 Day 05
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 40.702ms
+Time part 2: 34.346ms
+Both parts: 75.048349ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 8/50
-Total time: 1344.063ms
+Total stars: 10/50
+Total time: 115.356ms
 ```
 
 <!--/RESULTS-->
--- src/day05/index.ts
+++ src/day05/index.ts
@@ -1,19 +1,109 @@
 import run from "aocrunner";
+import _ from 'lodash'
 
-const parseInput = (rawInput: string) => rawInput.split('\n');
+const parseInput = (rawInput: string, diagonals = false) => rawInput.split('\n').map(l =>
+  l.split(' -> ').map(p => p.split(',').map(xy => parseInt(xy)))
+).filter(segment => {
+  if (diagonals) return true;
+  let start = segment[0];
+  let end = segment[1];
+  return start[0] === end[0] || start[1] == end[1]
+});
+
+const drawLine = (grid: number[][], line: number[][]) => {
+  let startPoint = line[0]
+  let endPoint = line[1]
+  if (startPoint[0] === endPoint[0]) {
+    // same x coordinate, so vertical line
+    // check whether we need to start from the top (start < end) or the bottom (start > end)
+    let start = startPoint[1] < endPoint[1] ? startPoint[1] : endPoint[1]
+    let end = startPoint[1] < endPoint[1] ? endPoint[1] : startPoint[1]
+    // console.log('start/end points', startPoint, endPoint)
+    // console.log('start/end', start, end)
+    for (let i = start; i <= end; i++) {
+      grid[startPoint[0]][i]++
+    }
+  } else if (startPoint[1] === endPoint[1]) {
+    // same y coordinate, so horizontal line
+    // check whether we need to start from the left (start < end) or the right (start > end)
+    let start = startPoint[0] < endPoint[0] ? startPoint[0] : endPoint[0]
+    let end = startPoint[0] < endPoint[0] ? endPoint[0] : startPoint[0]
+    // console.log('start/end points', startPoint, endPoint)
+    // console.log('start/end', start, end)
+    for (let i = start; i <= end; i++) {
+      grid[i][startPoint[1]]++
+    }
+  } else {
+    let xDirection = Math.sign(endPoint[0] - startPoint[0])
+    let yDirection = Math.sign(endPoint[1] - startPoint[1])
+    let x = startPoint[0];
+    let y = startPoint[1];
+    for (let i = 0; i < Math.abs(endPoint[0] - startPoint[0]) + 1; i++) {
+      grid[x][y]++
+      x += xDirection
+      y += yDirection
+    }
+
+  }
+  return grid
+}
 
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
-  console.log(input)
+  // get the largest x and y. That will form the initial grid size
+  let gridSize = input.reduce((acc, segment) => {
+    let maxX = _.max(segment.map(point => point[0])) as number
+    let maxY = _.max(segment.map(point => point[1])) as number
+    if (maxX > acc.x) {
+      acc.x = maxX
+    } if (maxY > acc.y) {
+      acc.y = maxY
+    }
+    return acc
+  }, { x: 0, y: 0 });
 
-  return;
+  // fill grid with 0's
+  let grid: number[][] = new Array(gridSize.x + 1)
+  for (let i = 0; i <= gridSize.x; i++) {
+    grid[i] = new Array(gridSize.y + 1)
+    for (let j = 0; j <= gridSize.y; j++) {
+      grid[i][j] = 0
+    }
+  }
+
+  grid = input.reduce((grid, segment) => drawLine(grid, segment), grid)
+
+  // count how many points have > 1
+  return grid.reduce((sum, row) => sum + row.filter(c => c > 1).length, 0)
 };
 
 const part2 = (rawInput: string) => {
-  const input = parseInput(rawInput);
-  console.log(input)
+  const input = parseInput(rawInput, true);
+  // get the largest x and y. That will form the initial grid size
+  let gridSize = input.reduce((acc, segment) => {
+    let maxX = _.max(segment.map(point => point[0])) as number
+    let maxY = _.max(segment.map(point => point[1])) as number
+    if (maxX > acc.x) {
+      acc.x = maxX
+    } if (maxY > acc.y) {
+      acc.y = maxY
+    }
+    return acc
+  }, { x: 0, y: 0 });
 
-  return;
+  // fill grid with 0's
+  let grid: number[][] = new Array(gridSize.x + 1)
+  for (let i = 0; i <= gridSize.x; i++) {
+    grid[i] = new Array(gridSize.y + 1)
+    for (let j = 0; j <= gridSize.y; j++) {
+      grid[i][j] = 0
+    }
+  }
+
+  grid = input.reduce((grid, segment) => drawLine(grid, segment), grid)
+
+  // count how many points have > 1
+  return grid.reduce((sum, row) => sum + row.filter(c => c > 1).length, 0)
 };
 
 let input: string = `0,9 -> 5,9
@@ -41,11 +131,11 @@ run({
     tests: [
       {
         input,
-        expected: "",
+        expected: 12,
       },
     ],
     solution: part2,
   },
   trimTestInputs: false,
-  onlyTests: true,
+  onlyTests: false,
 });
